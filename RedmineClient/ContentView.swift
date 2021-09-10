347bdce02ea99ef21303085ea319d3028d6f3af7
ernest0n <magatar136@yandex.ru>
2021-09-10 21:20:16 +0300

    Initial Commit

---
+++ RedmineClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RedmineClient
+//
+//  Created by Ernest Babayan on 10.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RedmineClient/RedmineClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedmineClientApp.swift
+//  RedmineClient
+//
+//  Created by Ernest Babayan on 10.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct RedmineClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
