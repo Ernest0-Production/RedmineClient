28affeb2bab294c457ba53a573a513e73ee1f62b
ernest0n <magatar136@yandex.ru>
2021-09-17 23:40:37 +0300

    Написал статью https://telegra.ph/Zero-Dependency-09-17

--- RedmineClient/ContentView.swift
+++ RedmineClient/ContentView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    let items = [1,2,3]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { proxy in
+            ScrollView {
+                ForEach(Array(repeating: 100, count: 100), id: \.self, content: { (number: Int) in
+                    Text(String(number))
+                        .frame(minWidth: proxy.size.width)
+                })
+            }
+        }
+        
     }
 }
 
