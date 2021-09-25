9acb61f1bceafcb0f37d5b78337ba9c7bf93d1af
ernest0n <magatar136@yandex.ru>
2021-09-25 14:56:15 +0300

    Рефакторит маппинг из ApiEndpoint в HTTPRequest

--- RedmineClient/Infrastracture/HTTPRequestFromApiEndpointBuilder.swift
+++ RedmineClient/Infrastracture/HTTPRequestFromApiEndpointBuilder.swift
@@ -17,72 +17,72 @@ extension Endpoint {
     }
 
     func asHTTPRequest(domain: String) -> Result<HTTPRequest, HTTPRequestBuilderError> {
-        Result<Data?, HTTPRequestBuilderError>.create({
-            switch task {
-            case Endpoint.Task.none, Endpoint.Task.query:
-                return Result.success(nil)
+        let body: Data?
 
-            case let Endpoint.Task.formData(dictionary):
-                return Result.success(
-                    HTTPFormData(dictionary.compactMapValues({ $0 }))
+        switch task {
+        case Endpoint.Task.none, Endpoint.Task.query:
+            body = nil
+
+        case let Endpoint.Task.formData(dictionary):
+            body = HTTPFormData(dictionary.compactMapValues({ $0 }))
+
+        case let Endpoint.Task.json(dictionary):
+            do {
+                body = try JSONSerialization.data(
+                    withJSONObject: dictionary.compactMapValues({ $0 }),
+                    options: []
                 )
+            } catch {
+                return Result.failure(HTTPRequestBuilderError.invalidJson(error))
+            }
+        }
 
-            case let Endpoint.Task.json(dictionary):
-                return Result(catching: {
-                    try JSONSerialization.data(
-                        withJSONObject: dictionary.compactMapValues({ $0 }),
-                        options: []
+        let request = HTTPRequest(
+            host: domain,
+
+            path: self.path,
+
+            method: conditional({
+                switch self.method {
+                case Endpoint.Method.get: return HTTPRequest.Method.get
+                case Endpoint.Method.post: return HTTPRequest.Method.post
+                case Endpoint.Method.delete: return HTTPRequest.Method.delete
+                case Endpoint.Method.put: return HTTPRequest.Method.put
+                }
+            }),
+
+            query: conditional({
+                guard case let Endpoint.Task.query(dictionary) = self.task else {
+                    return []
+                }
+
+                return dictionary.compactMap({
+                    guard let value = $0.value else { return nil }
+
+                    return HTTPRequest.Query(
+                        key: $0.key,
+                        value: String(describing: value)
                     )
                 })
-                .mapError(HTTPRequestBuilderError.invalidJson)
-            }
-        })
-        .map({ (body: Data?) in
-            HTTPRequest(
-                host: domain,
+            }),
 
-                path: self.path,
+            headers: conditional({
+                switch self.task {
+                case Endpoint.Task.none, Endpoint.Task.query:
+                    return [:]
 
-                method: conditional({
-                    switch self.method {
-                    case Endpoint.Method.get: return HTTPRequest.Method.get
-                    case Endpoint.Method.post: return HTTPRequest.Method.post
-                    case Endpoint.Method.delete: return HTTPRequest.Method.delete
-                    case Endpoint.Method.put: return HTTPRequest.Method.put
-                    }
-                }),
+                case Endpoint.Task.formData:
+                    return ["Content-Type": "application/x-www-form-urlencoded; charset=utf-8"]
 
-                query: conditional({
-                    guard case let Endpoint.Task.query(dictionary) = self.task else {
-                        return []
-                    }
+                case Endpoint.Task.json:
+                    return ["Content-Type": "application/json"]
+                }
+            }),
 
-                    return dictionary.compactMap({
-                        guard let value = $0.value else { return nil }
+            body: body
+        )
 
-                        return HTTPRequest.Query(
-                            key: $0.key,
-                            value: String(describing: value)
-                        )
-                    })
-                }),
-
-                headers: conditional({
-                    switch self.task {
-                    case Endpoint.Task.none, Endpoint.Task.query:
-                        return [:]
-
-                    case Endpoint.Task.formData:
-                        return ["Content-Type": "application/x-www-form-urlencoded; charset=utf-8"]
-
-                    case Endpoint.Task.json:
-                        return ["Content-Type": "application/json"]
-                    }
-                }),
-
-                body: body
-            )
-        })
+        return Result.success(request)
     }
 }
 
